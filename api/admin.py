fb169c4213a5e1a8589c597c21d886d0dfaf1f9b
tburakonat <tburakonat@gmail.com>
2024-12-23 20:20:23 +0100

    create models for the project

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Course)
+admin.site.register(models.Presentation)
+admin.site.register(models.Feedback)
+admin.site.register(models.FeedbackComment)
