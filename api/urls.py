19905d0a14a04d5265c1edfec3e34c14d9c7caba
tburakonat <tburakonat@gmail.com>
2024-12-26 14:17:47 +0100

    refactor API views to use viewsets and router for cleaner URL management

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path
 from api import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('users/', views.UserListCreate.as_view()),
-    path('users/<int:user_id>/', views.UserRetrieveUpdateDelete.as_view()),
-    path('courses/', views.CourseListCreate.as_view()),
-    path('courses/<int:course_id>/', views.CourseRetrieveUpdateDelete.as_view()),
-    path('presentations/', views.PresentationListCreate.as_view()),
-    path('presentations/<int:presentation_id>/', views.PresentationRetrieveUpdateDelete.as_view()),
-    path('presentations/<int:presentation_id>/feedback/', views.FeedbackListCreate.as_view()),
-    path('presentations/<int:presentation_id>/feedback/<int:feedback_id>/', views.FeedbackRetrieveUpdateDelete.as_view()),
-    path('feedback/<int:feedback_id>/comments/', views.FeedbackCommentListCreate.as_view()),
-    path('feedback/<int:feedback_id>/comments/<int:comment_id>/', views.FeedbackCommentRetrieveUpdateDelete.as_view()),
-]
+router = DefaultRouter()
+
+router.register('users', views.UserViewSet)
+router.register('courses', views.CourseViewSet)
+router.register('presentations', views.PresentationViewSet)
+router.register('feedbacks', views.FeedbackViewSet)
+router.register('comments', views.FeedbackCommentViewSet)
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import NotFound
+from rest_framework import viewsets
 from api.filters import (
     UserFilter, 
     CourseFilter,
@@ -27,169 +27,62 @@ from api.serializers import (
 )
 
 
-class UserListCreate(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filterset_class = UserFilter
 
     def get_permissions(self):
         self.permission_classes = [IsAuthenticated]
-        if self.request.method == 'POST':
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
 
 
-class UserRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    lookup_url_kwarg = 'user_id'
-    lookup_field = 'id'
-
-    def get_permissions(self):
-        self.permission_classes = [IsAuthenticated]
-        if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            self.permission_classes = [IsAdminUser]
-        return super().get_permissions()
-
-
-class CourseListCreate(generics.ListCreateAPIView):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filterset_class = CourseFilter
 
     def get_permissions(self):
         self.permission_classes = [IsAuthenticated]
-        if self.request.method == 'POST':
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
 
 
-class CourseRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    lookup_url_kwarg = 'course_id'
-    lookup_field = 'id'
-
-    def get_permissions(self):
-        self.permission_classes = [IsAuthenticated]
-        if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            self.permission_classes = [IsAdminUser]
-        return super().get_permissions()
-
-
-class PresentationListCreate(generics.ListCreateAPIView):
+class PresentationViewSet(viewsets.ModelViewSet):
     queryset = Presentation.objects.all()
     serializer_class = PresentationSerializer
     filterset_class = PresentationFilter
 
     def get_permissions(self):
         self.permission_classes = [IsAuthenticated]
-        if self.request.method == 'POST':
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
 
 
-class PresentationRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Presentation.objects.all()
-    serializer_class = PresentationSerializer
-    lookup_url_kwarg = 'presentation_id'
-    lookup_field = 'id'
-
-    def get_permissions(self):
-        self.permission_classes = [IsAuthenticated]
-        if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            self.permission_classes = [IsAdminUser]
-        return super().get_permissions()
-
-
-class FeedbackListCreate(generics.ListCreateAPIView):
+class FeedbackViewSet(viewsets.ModelViewSet):
+    queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
     filterset_class = FeedbackFilter
 
-    def get_queryset(self):
-        presentation_id = self.kwargs.get('presentation_id')
-        queryset = Feedback.objects.filter(presentation_id=presentation_id)
-        if not queryset.exists():
-            raise NotFound("No Feedback matches the given query.")
-        return queryset
-
-    
     def get_permissions(self):
         self.permission_classes = [IsAuthenticated]
-        if self.request.method == 'POST':
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
 
 
-class FeedbackRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = FeedbackSerializer
-    lookup_url_kwarg = 'feedback_id'
-    lookup_field = 'id'
-
-    def get_permissions(self):
-        self.permission_classes = [IsAuthenticated]
-        if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            self.permission_classes = [IsAdminUser]
-        return super().get_permissions()
-
-
-    def get_queryset(self):
-        presentation_id = self.kwargs.get('presentation_id')
-        return Feedback.objects.filter(presentation_id=presentation_id)
-
-
-    def get_object(self):
-        queryset = self.get_queryset()
-        feedback_id = self.kwargs.get('feedback_id')
-        
-        try:
-            obj = queryset.get(id=feedback_id)
-        except Feedback.DoesNotExist:
-            raise NotFound(detail="Feedback not found.")
-        
-        return obj
-
-
-class FeedbackCommentListCreate(generics.ListCreateAPIView):
+class FeedbackCommentViewSet(viewsets.ModelViewSet):
+    queryset = FeedbackComment.objects.all()
     serializer_class = FeedbackCommentSerializer
     filterset_class = FeedbackCommentFilter
 
-    def get_queryset(self):
-        feedback_id = self.kwargs.get('feedback_id')
-        queryset = FeedbackComment.objects.filter(feedback_id=feedback_id)
-        if not queryset.exists():
-            raise NotFound("No FeedbackComment matches the given query.")
-        return queryset
-    
     def get_permissions(self):
         self.permission_classes = [IsAuthenticated]
-        if self.request.method == 'POST':
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
-
-
-class FeedbackCommentRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = FeedbackCommentSerializer
-
-    def get_permissions(self):
-        self.permission_classes = [IsAuthenticated]
-        if self.request.method in ["PUT", "PATCH", "DELETE"]:
-            self.permission_classes = [IsAdminUser]
-        return super().get_permissions()
-
-
-    def get_queryset(self):
-        feedback_id = self.kwargs.get('feedback_id')
-        return FeedbackComment.objects.filter(feedback_id=feedback_id)
-
-    def get_object(self):
-        queryset = self.get_queryset()
-        comment_id = self.kwargs.get('comment_id')
-        
-        try:
-            obj = queryset.get(id=comment_id)
-        except FeedbackComment.DoesNotExist:
-            raise NotFound(detail="Comment not found.")
-
-        return obj
 
